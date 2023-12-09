90b92e204318b99571ca2dc1e20b4a32ccf4c923
Tiago Roberto <tiagowebwork@gmail.com>
2023-12-09 06:18:36 -0300

    initial commit

---
+++ src/test/java/com/teste/scrd/ScrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.scrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
